224c9bd2abfab2acf9a0880240ba626704bd4296
Srayash <146334722+Srayash@users.noreply.github.com>
2024-09-25 23:15:04 +0530

    Update currentWeather.js

--- currentWeather.js
+++ currentWeather.js
@@ -67,17 +67,17 @@ async function checkWeather(city){
         document.querySelector(`li .day-${i+1}`).innerHTML = data.list[hourLeft/2 + i*8].dt_txt.substr(0,10);
         document.querySelector(`li .temp-${i+1}`).innerHTML = Math.round(data.list[hourLeft/2 + i*8].main.temp) + "°C";
         document.querySelector(`li .weather-${i+1}`).innerHTML = data.list[hourLeft/2 + i*8].weather[0].main;
-        if(data.list[i+1].weather[0].main == "Clouds"){
-            document.querySelector(`li .img-${i+1}`).src = "./Icons/Cloudy.png"
-        }
-        else if(data.list[i+1].weather[0].main == "Rain"){
-            document.querySelector(`li .img-${i+1}`).src = "./Icons/Rain.png"
-        }
-        else if(data.list[i+1].weather[0].main == "Thunderstorm"){
-            document.querySelector(`li .img-${i+1}`).src = "./Icons/Bolt.png"
-        }
-        else if(data.list[i+1].weather[0].main == "Clear"){
-            document.querySelector(`li .img-${i+1}`).src = "./Icons/Sunny.png"
+        if(data.list[0].weather[0].main == "Clouds"){
+        weatherIcon.src = "./Icons/Cloudy.png"
+    }
+    else if(data.list[0].weather[0].main == "Rain"){
+        weatherIcon.src = "./Icons/Rain.png"
+    }
+    else if(data.list[0].weather[0].main == "Thunderstorm"){
+        weatherIcon.src = "./Icons/Bolt.png"
+    }
+    else if(data.list[0].weather[0].main == "Clear"){
+        weatherIcon.src = "./Icons/Sunny.png"
         }
     }
 }}
